727c61b1c375ddaf66ddceba087569cce1f3dd2f
Mato <tissuepaperarelimited@gmail.com>
2022-08-31 22:18:03 +0800

    On my way

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by kittawat phuangsombat on 2022/8/30.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    
+
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by kittawat phuangsombat on 2022/8/30.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+    
+
+
+}
