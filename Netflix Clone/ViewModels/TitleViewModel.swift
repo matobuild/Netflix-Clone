a5bd9dc7b0aabc2d4658af4fc3d8a14b19131405
Mato <tissuepaperarelimited@gmail.com>
2022-09-03 17:21:34 +0800

    -impliment upcoming and search page

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by kittawat phuangsombat on 2022/9/2.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
